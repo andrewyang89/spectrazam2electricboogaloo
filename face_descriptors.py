b716b58470645e78d2cc4648ff530caf3f2135b2
andrewyang89 <andrewyang89@outlook.com>
2020-07-16 16:38:59 -0400

    Adds incomplete labeling method and sample image

--- face_descriptors.py
+++ face_descriptors.py
@@ -44,8 +44,7 @@ def show_boxes(model, pic):
 
     for box, prob, landmark in zip(boxes, probabilities, landmarks):
         # draw the box on the screen
-        ax.add_patch(Rectangle(box[:2], *(box[2:] - box[:2]), fill=None, lw=2, color="red"))
-
+        ax.add_patch(Rectangle(box[:2], *(box[2:] - box[:2]), fill=None, lw=2, color="green"))
         # Get the landmarks/parts for the face in box d.
         # Draw the face landmarks on the screen.
         for i in range(len(landmark)):
@@ -53,3 +52,32 @@ def show_boxes(model, pic):
 
     plt.show()
     return boxes
+
+def show_boxes_labels(model, pic):
+    """
+    Plots the image with the boxes applied and returns the boxes needed to compute the descriptors
+    :param model: the FaceNet model
+    :param pic: the picture to be processed
+    :return: the boxes (detected faces)
+    """
+    fig, ax = plt.subplots()
+    ax.imshow(pic)
+
+    boxes, probabilities, landmarks = model.detect(pic)
+    # labels = match(pic)
+
+    for box, prob, landmark, label in zip(boxes, probabilities, landmarks, labels):
+        # draw the box on the screen
+        ax.add_patch(Rectangle(box[:2], *(box[2:] - box[:2]), fill=None, lw=2, color="green"))
+        if label is None:
+            ax.annotate("Unknown", box[:2], color="red")
+        else:
+            ax.annotate(label, box[:2], color="blue")
+        # Get the landmarks/parts for the face in box d.
+        # Draw the face landmarks on the screen.
+        for i in range(len(landmark)):
+            ax.plot(landmark[i, 0], landmark[i, 1], '+', color="blue")
+
+    plt.show()
+    return boxes
+
